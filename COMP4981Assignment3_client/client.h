1d950993668eda5071641bc1d58d2eddb60ec039
Daniel Shin <danielshin234@gmail.com>
2019-03-14 17:26:18 -0700

    got threads to work in clion for client

---
+++ COMP4981Assignment3_client/client.h
@@ -0,0 +1,30 @@
+#ifndef COMP4981ASSIGNMENT3_CLIENT_CLIENT_HPP
+#define COMP4981ASSIGNMENT3_CLIENT_CLIENT_HPP
+
+#include <stdio.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#define SERVER_TCP_PORT 7000 // Default port
+#define BUFLEN 200
+
+int n, bytes_to_read;
+int sd, port;
+struct hostent* hp;
+struct sockaddr_in server;
+char* host, * bp, rbuf[BUFLEN], sbuf[BUFLEN], ** pptr;
+char str[16];
+
+int enterClient( char* hostName );
+
+void* myThreadFun( void* vargp );
+
+#endif //COMP4981ASSIGNMENT3_CLIENT_CLIENT_HPP
